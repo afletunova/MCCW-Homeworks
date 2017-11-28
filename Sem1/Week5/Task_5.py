18910411923096ab429c3e77cfa7a930a5eb2339
Afletunova <afletunova.yulia@gmail.com>
2017-11-28 12:14:40 +0300

    Change parameters

--- Sem1/Week5/Task_5.py
+++ Sem1/Week5/Task_5.py
@@ -3,11 +3,14 @@ import scipy.integrate as ig
 
 #1
 A = 0
-B = 1
-m = 10
+#B = math.pi / 2
+B = 50
+m = 10000
 
 w = lambda x: 1
-f = lambda x: math.exp(-x) - x ** 2 / 2
+g = lambda x: math.sin(x) - x ** 5 / 2
+
+f = lambda x: w(x) * g(x)
 
 #2
 J = ig.quad(f, A, B)[0]
@@ -33,15 +36,15 @@ com_qf_tr = h / 2 * (f(A) + f(B))
 sum = 0
 for i in range(1, m):
     sum += f(points[i])
-
 com_qf_tr += h * sum
+
 print('Composite Quadratic formula of trapezium:', com_qf_tr)
 print('|J - J(h)| =', abs(J - com_qf_tr))
 
 com_qf_simp = 0
 for i in range(0, m):
     com_qf_simp += f(points[i]) + f(points[i + 1]) + 4 * f((points[i] + points[i + 1]) / 2)
-
 com_qf_simp *= h / 6
+
 print('Composite Quadratic Simpson formula:', com_qf_simp)
 print('|J - J(h)| =', abs(J - com_qf_simp))
